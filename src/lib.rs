6539346f29af6bfe518f8495d6a7c39472e0b7a4
Zoe Soutter <zoe.soutter.com>
2024-11-01 02:24:27 +0000

    Add support for join read-writer

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,58 @@ use std::{
 use serde::{de::DeserializeOwned, Serialize};
 use type_hash::TypeHash;
 
+
+pub struct MessageReadWriteStream<SendMessageType: Serialize, RecvMessageType: DeserializeOwned, ReadWriter: Read + Write,> {
+    send_msg_type: PhantomData<SendMessageType>,
+    recv_msg_type: PhantomData<RecvMessageType>,
+    readwriter: ReadWriter,
+}
+impl<SendMessageType: Serialize + TypeHash, RecvMessageType: DeserializeOwned + TypeHash, ReadWriter: Read + Write> MessageReadWriteStream<SendMessageType, RecvMessageType, ReadWriter> {
+    pub fn new(mut readwriter: ReadWriter) -> std::io::Result<Self> {
+        let mut hasher = DefaultHasher::new();
+
+        SendMessageType::type_hash().hash(&mut hasher);
+        RecvMessageType::type_hash().hash(&mut hasher);
+        let sent_hash = hasher.finish();
+        readwriter.write_all(&sent_hash.to_be_bytes())?;
+
+        let mut hasher = DefaultHasher::new();
+        RecvMessageType::type_hash().hash(&mut hasher);
+        SendMessageType::type_hash().hash(&mut hasher);
+        let expected_hash = hasher.finish();
+        let mut actual_hash = [0u8; 8];
+        readwriter.read_exact(&mut actual_hash)?;
+
+        if expected_hash.to_be_bytes() == actual_hash {
+            Ok(Self {
+                send_msg_type: PhantomData::default(),
+                recv_msg_type: PhantomData::default(),
+                readwriter,
+            })
+        } else {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Peer using a different message type (This could even be different type names, if you require this difference, use `new_unchecked()`)",
+            ))
+        }
+    }
+}
+impl<SendMessageType: Serialize, RecvMessageType: DeserializeOwned, ReadWriter: Write + Read> MessageReadWriteStream<SendMessageType, RecvMessageType, ReadWriter> {
+    pub fn new_unchecked(readwriter: ReadWriter) -> Self {
+        Self {
+            send_msg_type: PhantomData::default(),
+            recv_msg_type: PhantomData::default(),
+            readwriter,
+        }
+    }
+    pub fn send(&mut self, message: SendMessageType) -> Result<(), Box<bincode::ErrorKind>> {
+        bincode::serialize_into(&mut self.readwriter, &message)
+    }
+    pub fn read(&mut self) -> Result<RecvMessageType, Box<bincode::ErrorKind>> {
+        bincode::deserialize_from(&mut self.readwriter)
+    }
+}
+
 pub struct MessageStream<SendMessageType: Serialize, RecvMessageType: DeserializeOwned, Reader: Read, Writer: Write> {
     send_msg_type: PhantomData<SendMessageType>,
     recv_msg_type: PhantomData<RecvMessageType>,
